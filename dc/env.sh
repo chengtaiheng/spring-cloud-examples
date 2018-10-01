d496be43c81db4bc4dcf1ad914905e63f8178caa
应卓 <yingzhor@gmail.com>
2018-10-01 10:43:42 +0800

    update

---
+++ dc/env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Eureka
+EUREKA_HOST=eureka
+EUREKA_PORT=8761
+
+# MySQL
+MYSQL_HOST=10.211.55.26
+MYSQL_PORT=3306
--- rebuild.sh
+++ rebuild.sh
@@ -9,9 +9,10 @@ docker rmi sce/node-eureka:latest
 docker rmi sce/node-apigateway:1.0.0
 docker rmi sce/node-apigateway:latest
 
-mvn -f ./node-eureka/pom.xml clean package docker:build
-mvn -f ./node-service-provider/pom.xml clean package docker:build
-mvn -f ./node-service-consumer/pom.xml clean package docker:build
-mvn -f ./node-apigateway/pom.xml clean package docker:build
+mvn -f ./pom.xml install
+mvn -f ./node-eureka/pom.xml            docker:build
+mvn -f ./node-service-provider/pom.xml  docker:build
+mvn -f ./node-service-consumer/pom.xml  docker:build
+mvn -f ./node-apigateway/pom.xml        docker:build
 
 mvn clean -q
