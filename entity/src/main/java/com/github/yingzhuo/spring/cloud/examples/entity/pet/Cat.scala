601406600a1cb905f8758b5c5fc700efaede0cad
应卓 <yingzhor@gmail.com>
2018-10-05 11:41:46 +0800

    update

---
+++ entity/src/main/java/com/github/yingzhuo/spring/cloud/examples/entity/pet/Cat.scala
@@ -0,0 +1,44 @@
+package com.github.yingzhuo.spring.cloud.examples.entity.pet
+
+import java.util.Date
+
+import javax.persistence._
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+import scala.beans.BeanProperty
+
+object Cat {
+}
+
+/**
+  * 猫
+  */
+@Entity
+@Table(name = "t_cat")
+@EntityListeners(Array(classOf[AuditingEntityListener]))
+class Cat extends Pet {
+
+  @Id
+  @Column(name = "id")
+  @BeanProperty
+  var id: String = _
+
+  @Column(name = "name")
+  @BeanProperty
+  var name: String = _
+
+  @Column(name = "keeper")
+  @BeanProperty
+  var keeper: String = _
+
+  @Column(name = "created_time")
+  @Temporal(TemporalType.TIMESTAMP)
+  @BeanProperty
+  var createdTime: Date = _
+
+  @Column(name = "last_modified_time")
+  @Temporal(TemporalType.TIMESTAMP)
+  @BeanProperty
+  var lastModifiedTime: Date = _
+
+}
--- entity/src/main/java/com/github/yingzhuo/spring/cloud/examples/entity/pet/Pet.scala
+++ entity/src/main/java/com/github/yingzhuo/spring/cloud/examples/entity/pet/Pet.scala
@@ -1,32 +1,11 @@
 package com.github.yingzhuo.spring.cloud.examples.entity.pet
 
-import javax.persistence.{Column, Entity, Id, Table}
-
-import scala.beans.BeanProperty
-
 /**
   * 宠物
   */
-sealed trait Pet extends Serializable
-
-object Cat {
+object Pet {
 }
 
-@Entity
-@Table(name = "t_cat")
-class Cat extends Pet {
+trait Pet extends Serializable
 
-  @Id
-  @Column(name = "id")
-  @BeanProperty
-  var id: String = _
 
-  @Column(name = "name")
-  @BeanProperty
-  var name: String = _
-
-  @Column(name = "keeper")
-  @BeanProperty
-  var keeper: String = _
-
-}
