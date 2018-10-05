44964101c512d4a9828dedf72fa539c33cb67676
应卓 <yingzhor@gmail.com>
2018-10-05 11:52:15 +0800

    update

--- node-service-provider/src/main/java/com/github/yingzhuo/spring/cloud/examples/provider/ApplicationCnfJpa.scala
+++ node-service-provider/src/main/java/com/github/yingzhuo/spring/cloud/examples/provider/ApplicationCnfJpa.scala
@@ -1,15 +1,18 @@
 package com.github.yingzhuo.spring.cloud.examples.provider
 
+import java.util.Optional
+
 import javax.persistence.EntityManagerFactory
 import javax.sql.DataSource
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.{EnableJpaAuditing, EnableJpaRepositories}
 import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableJpaRepositories
 @EnableTransactionManagement
 @EntityScan(Array("com.github.yingzhuo.spring.cloud.examples"))
@@ -21,6 +24,17 @@ class ApplicationCnfJpa {
     val manager = new JpaTransactionManager(factory)
     manager.setDataSource(dataSource)
     manager
+  }
+
+  @Bean
+  def auditorAware(): AuditorAware[String] = ApplicationCnfJpa.DefaultAuditorAware
+
+}
+
+object ApplicationCnfJpa {
+
+  object DefaultAuditorAware extends AuditorAware[String] {
+    override def getCurrentAuditor: Optional[String] = Optional.of("system")
   }
 
 }
