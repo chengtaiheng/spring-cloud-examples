e01fcae48a27ccdde47270b07eb189a93445c5de
yingzhuo <yingzhor@gmail.com>
2018-11-14 09:21:40 +0800

    update

--- rebuild.sh
+++ rebuild.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-docker rmi sce/node-service-consumer:1.0.0
-docker rmi sce/node-service-consumer:latest
-docker rmi sce/node-service-provider:1.0.0
-docker rmi sce/node-service-provider:latest
-docker rmi sce/node-eureka:1.0.0
-docker rmi sce/node-eureka:latest
-docker rmi sce/node-apigateway:1.0.0
-docker rmi sce/node-apigateway:latest
-docker rmi sce/node-configuration:1.0.0
-docker rmi sce/node-configuration:latest
+mvn clean package -Dmaven.test.skip=true
 
-mvn -f ./pom.xml install
-mvn -f ./node-eureka/pom.xml            docker:build
-mvn -f ./node-service-provider/pom.xml  docker:build
-mvn -f ./node-service-consumer/pom.xml  docker:build
-mvn -f ./node-apigateway/pom.xml        docker:build
-mvn -f ./node-configuration/pom.xml     docker:build
+read -p "您是否需要构建'node-eureka'的镜像? (y/n) " build
+if [[ "y" == ${build} || "Y" == ${build} ]]; then
+    TAG="sce/node-eureka:latest"
+    mv ./node-eureka/target/node-eureka.jar ./app.jar
+    cp ./node-eureka/src/main/docker/Dockerfile ./Dockerfile
+    docker build -t ${TAG} .
+    unset TAG
+fi
+unset build
 
-mvn clean -q
+#mvn clean -q
