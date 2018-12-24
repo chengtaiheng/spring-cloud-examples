0563c6acd235f624333783b7961e8ae6bbed96e2
yingzhuo <yingzhor@gmail.com>
2018-12-24 14:28:10 +0800

    update

--- image-build.sh
+++ image-build.sh
@@ -12,11 +12,13 @@ if [[ "y" == ${build} || "Y" == ${build} ]]; then
     TAG="sce/node-eureka:latest"
     mv ./node-eureka/target/app.jar ./app.jar
     cp ./node-eureka/src/main/docker/Dockerfile ./Dockerfile
+    cp ./node-eureka/src/main/docker/entrypoint.sh ./entrypoint.sh
     docker build -t ${TAG} .
 
     unset TAG
     rm ./*.jar
-    rm Dockerfile
+    rm ./Dockerfile
+    rm ./entrypoint.sh
 fi
 unset build
 
@@ -25,11 +27,13 @@ if [[ "y" == ${build} || "Y" == ${build} ]]; then
     TAG="sce/node-configuration:latest"
     mv ./node-configuration/target/app.jar ./app.jar
     cp ./node-configuration/src/main/docker/Dockerfile ./Dockerfile
+    cp ./node-configuration/src/main/docker/entrypoint.sh ./entrypoint.sh
     docker build -t ${TAG} .
 
     unset TAG
     rm ./*.jar
-    rm Dockerfile
+    rm ./Dockerfile
+    rm ./entrypoint.sh
 fi
 unset build
 
@@ -38,11 +42,13 @@ if [[ "y" == ${build} || "Y" == ${build} ]]; then
     TAG="sce/node-service-provider:latest"
     mv ./node-service-provider/target/app.jar ./app.jar
     cp ./node-service-provider/src/main/docker/Dockerfile ./Dockerfile
+    cp ./node-service-provider/src/main/docker/entrypoint.sh ./entrypoint.sh
     docker build -t ${TAG} .
 
     unset TAG
     rm ./*.jar
-    rm Dockerfile
+    rm ./Dockerfile
+    rm ./entrypoint.sh
 fi
 unset build
 
@@ -51,11 +57,13 @@ if [[ "y" == ${build} || "Y" == ${build} ]]; then
     TAG="sce/node-service-consumer:latest"
     mv ./node-service-consumer/target/app.jar ./app.jar
     cp ./node-service-consumer/src/main/docker/Dockerfile ./Dockerfile
+    cp ./node-service-consumer/src/main/docker/entrypoint.sh ./entrypoint.sh
     docker build -t ${TAG} .
 
     unset TAG
     rm ./*.jar
-    rm Dockerfile
+    rm ./Dockerfile
+    rm ./entrypoint.sh
 fi
 unset build
 
@@ -64,11 +72,13 @@ if [[ "y" == ${build} || "Y" == ${build} ]]; then
     TAG="sce/node-apigateway:latest"
     mv ./node-apigateway/target/app.jar ./app.jar
     cp ./node-apigateway/src/main/docker/Dockerfile ./Dockerfile
+    cp ./node-apigateway/src/main/docker/entrypoint.sh ./entrypoint.sh
     docker build -t ${TAG} .
 
     unset TAG
     rm ./*.jar
-    rm Dockerfile
+    rm ./Dockerfile
+    rm ./entrypoint.sh
 fi
 unset build
 
